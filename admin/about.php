e2537ff258291c3137178ce356c5a2dae548afa7
JosephineBonka <josephinebonka123@gmail.com>
2022-07-19 07:28:56 +0300

    readme

---
+++ admin/about.php
@@ -0,0 +1,65 @@
+<?php
+require_once('check_login.php');
+?>
+<?php include"header.php"?>
+ 
+<?php include"sidebar.php"?>
+        
+        <div class="page-wrapper">
+            
+            <div class="row page-titles">
+                <div class="col-md-5 align-self-center">
+                    <h3 class="text-primary">About Elago Technologies Ltd</h3> </div>
+                <div class="col-md-7 align-self-center">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="javascript:void(0)">About Elago Technologies</a></li>
+                        <li class="breadcrumb-item active">Dashboard</li>
+                    </ol>
+                </div>
+            </div>
+            
+            
+            <div class="container-fluid">
+                
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="card card-outline-primary">
+                            
+                            <div class="card-body">
+                               
+                                    <div class="form-body">
+                                        <h3 class="card-title m-t-15"><strong>Contact Info</strong>   </h3>
+                                        <hr>
+                      <h4>                  Tel:   +256 701 376 017
+                        <br>
+                                            Email:    elagotechnologies@gmail.com
+                        </h4>      <br>
+                        <h3 class="card-title m-t-15"><strong>What We Do</strong>   </h3>
+                        <hr>
+                      <h4>                  Applications:  Web, Mobile, <br>
+
+                        </h4>      
+
+                         <br>
+                        <h3 class="card-title m-t-15"><strong>Support</strong>   </h3>
+                        <hr>
+                      <h4>                  Email:    elagotechnologies@gmail.com <br>
+                                            Email:    info@elagotechnologies.com
+                        </h4>  
+<br><br>
+                 
+                                      
+
+                                       
+                                    </div>
+                                   
+                               
+                            </div>
+                        </div>
+                
+ </div>
+</div>
+</div>
+                   
+           
+<?php include"footer.php"?>
